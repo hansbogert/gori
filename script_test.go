797719d236d6eb27ef587c27a6b0375701aed9a6
Hans van den Bogert <h.vandenbogert@guida.nl>
2025-08-31 17:42:51 +0200

    test: allow running git commands in tests

--- script_test.go
+++ script_test.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
 func TestMain(m *testing.M) {
-	os.Exit(testscript.RunMain(m, map[string]func() int{
+	commands := map[string]func() int{
 		"gori": Main,
-	}))
+		"git": func() int {
+			cmd := exec.Command("/usr/bin/git", os.Args[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				if exitError, ok := err.(*exec.ExitError); ok {
+					return exitError.ExitCode()
+				}
+				return 1
+			}
+			return 0
+		},
+	}
+	os.Exit(testscript.RunMain(m, commands))
 }
 
 func TestGori(t *testing.T) {
